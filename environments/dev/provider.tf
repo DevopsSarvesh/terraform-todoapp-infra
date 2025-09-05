a6895e6000f8a3bc9dbacf1263e98efbbfe9f293
kpsarvesh <kpsarvesh@outlook.com>
2025-09-05 17:37:12 +0530

    backend setup

--- environments/dev/provider.tf
+++ environments/dev/provider.tf
@@ -1,18 +1,18 @@
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
+      source = "hashicorp/azurerm"
       version = "4.42.0"
     }
   }
 
-  #   backend "azurerm" {
-  #     resource_group_name   = "rg-dev-todoapp"
-  #     storage_account_name  = "sadevtodoapp"
-  #     container_name        = "tfstate"
-  #     key                   = "dev.terraform.tfstate"
-
-  #   }
+  backend "azurerm" {
+    resource_group_name   = "rg-dev-todoapp"
+    storage_account_name  = "sagithub"
+    container_name        = "tfstate"
+    key                   = "dev.tfstate"
+    
+  }
 }
 
 provider "azurerm" {
